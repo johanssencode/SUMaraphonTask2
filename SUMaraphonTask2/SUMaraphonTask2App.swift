bc8c4dcb6323d7a3adeea6dc2bf9482084dbf469
Adelmaer <adelmaerr@gmail.com>
2024-12-04 16:49:37 +0500

    Initial Commit

---
+++ SUMaraphonTask2/SUMaraphonTask2App.swift
@@ -0,0 +1,17 @@
+//
+//  SUMaraphonTask2App.swift
+//  SUMaraphonTask2
+//
+//  Created by A on 04.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SUMaraphonTask2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
